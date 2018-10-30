4af46b9c1c74502a0644c0d9b326619b2eb02e1e
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-10-30 17:29:40 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,36 +1,28 @@
 class ProductsController < ApplicationController
-
-  def index
+   def index
     @products = Product.all
   end
-
-  def inventory
-    product = Product.find(params[:id])
-    render plain: product.inventory > 0 ? true : false
+   def new
+    @product = Product.new
   end
-
-  def description
+   def create
+    @product = Product.create(product_params)
+    redirect_to products_path
+  end
+   def inventory
+    product = Product.find(params[:id])
+    if product.inventory > 0
+    render plain: "true"
+  else
+    render plain: "false"
+  end
+  end
+   def description
     product = Product.find(params[:id])
     render plain: product.description
   end
-
-  def new
-    @product = Product.new
+   private
+   def product_params
+    params.require(:product).permit(:name, :price, :description, :inventory)
   end
-
-  def create
-      @product = Product.create(product_params)
-      if @product.save
-        redirect_to products_path
-      else
-        render product_new_path
-      end
-  end
-
-  private
-
-  def product_params
-    params.require(:product).permit(:name, :description, :inventory, :price)
-  end
-
-end
+end	end
